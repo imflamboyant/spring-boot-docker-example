f8cf0e744b7aa033d256ac4b6bded6ea9a7a6ffd
imflamboyant <m.djakovic@mdjakovic.levi9.com>
2020-06-15 00:27:23 +0200

    Add SpringBoot smoke tests to check if context loads

---
+++ front-api/src/test/com/example/demo/frontapi/FrontApiApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.frontapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class FrontApiApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
